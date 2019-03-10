a8e51d54c9b2f81a37cac0326abc2ac7eade4237
Thaniiaa <34150974+Thaniiaa@users.noreply.github.com>
2019-03-10 22:51:48 +0800

    Add files via upload

---
+++ status.php
@@ -0,0 +1,23 @@
+<?php
+ 
+$access_token = "Ganti Sama Tokenmu";
+$url = "https://graph.facebook.com/me/feed?method=POST";
+if(!empty($_GET['x'])){
+$status = $_GET['x'];
+} else {
+$send = file("http://kibass.com/kata.php");
+$nabila = $send[array_rand($send)];
+$statusku= $nabila; 
+} 
+    $ch = curl_init();
+    $attachment =  array(   'access_token'  => $access_token,                       
+                        'message'          => $statusku,
+                    );
+    curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $attachment);
+    $result= curl_exec($ch);
+    curl_close ($ch);
+?>
